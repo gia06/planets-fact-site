8415ee04e4d975c705681005d90db8ee85ed6b62
gia <gia6shamugia@gmail.com>
2023-02-27 07:33:57 -0800

    added type for planet data

--- src/components/Mercury.tsx
+++ src/components/Mercury.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import mercury from "../assets/planet-mercury.svg";
 
 function Mercury() {
   const navigate = useNavigate();
 
-  return <div>Mercury</div>;
+  return (
+    <PlanetContainer>
+      <img src={mercury} />
+    </PlanetContainer>
+  );
 }
 
 export default Mercury;
+
+const PlanetContainer = styled.div``;
---
+++ src/hooks/useFetch.tsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+import data from "../data/data.json";
+
+function useFetch() {
+  const [planetsData, setPlanetsData] = useState();
+  return;
+}
+
+export default useFetch;
